b3383977a64bf67ba5cd6bc5912b61a015ea39ed
Djamani Nadjim <djamaninadjim@gmail.com>
2025-05-21 08:29:46 +0200

    installation de cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const blagueRoutes = require('./routes/blagueRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./swagger/swaggerConfig');
 
+app.use(cors())
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use('/', blagueRoutes);
